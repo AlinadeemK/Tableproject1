d8688d6932019973ddd26829e40e34ce3ce81a66
Nadeem Ali <154249008+AlinadeemK@users.noreply.github.com>
2024-01-22 19:58:41 +0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,18 @@
+// Assuming you have a value for 'inp'
+var inp = 5; // 1
+
+// Display multiplication table using innerHTML
+document.write('$ {inp} x 1 = ', inp*1 + "<br>")
+
+// console.log
+
+// console.log(`${inp} x 1 = ${inp * 1}`);
+// console.log(`${inp} x 2 = ${inp * 2}`);
+// console.log(`${inp} x 3 = ${inp * 3}`);
+// console.log(`${inp} x 4 = ${inp * 4}`);
+// console.log(`${inp} x 5 = ${inp * 5}`);
+// console.log(`${inp} x 6 = ${inp * 6}`);
+// console.log(`${inp} x 7 = ${inp * 7}`);
+// console.log(`${inp} x 8 = ${inp * 8}`);
+// console.log(`${inp} x 9 = ${inp * 9}`);
+// console.log(`${inp} x 10 = ${inp * 10}`);
